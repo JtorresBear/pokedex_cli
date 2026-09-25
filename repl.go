7238346872f2d4775f09198017384c078c3a5c8f
Juan Torres <jmmtorres1991@yahoo.com>
2026-09-25 03:08:57 -0500

    finished some of the repl logic and added help and exit commands

--- repl.go
+++ repl.go
@@ -1,7 +1,67 @@
 package main
 
-func cleanInput(text string) []string {
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
 
-	emptyForNow := []string{}
-	return emptyForNow
+type cliCommand struct {
+	name        string
+	description string
+	callback    func() error
+}
+
+func startRepl() {
+	scanner := bufio.NewScanner(os.Stdin)
+	for {
+		fmt.Print("Pokedex > ")
+		if !scanner.Scan() {
+			if err := scanner.Err(); err != nil {
+				fmt.Printf("there was an error: %v", err)
+				break
+			}
+		}
+		input := scanner.Text()
+		words := cleanInput(input)
+		if len(words) == 0 {
+			continue
+		}
+		commandName := words[0]
+
+		command, exists := getCommands()[commandName]
+		if exists {
+			err := command.callback()
+			if err != nil {
+				fmt.Println(err)
+			}
+			continue
+		} else {
+			fmt.Println("Unknown command")
+			continue
+		}
+	}
+}
+
+func cleanInput(input string) []string {
+	input = strings.ToLower(input)
+	split_input := strings.Fields(input)
+
+	return split_input
+}
+
+func getCommands() map[string]cliCommand {
+	return map[string]cliCommand{
+		"help": {
+			name:        "help",
+			description: "Displays a help message",
+			callback:    commandHelp,
+		},
+		"exit": {
+			name:        "exit",
+			description: "Exit the Pokedex",
+			callback:    commandExit,
+		},
+	}
 }
--- repl_test.go
+++ repl_test.go
@@ -3,5 +3,40 @@ package main
 import "testing"
 
 func TestCleanInput(t *testing.T) {
+	cases := []struct {
+		input    string
+		expected []string
+	}{
+		{
+			input:    "Hello World!",
+			expected: []string{"hello", "world!"},
+		},
+		{
+			input:    "  This  is  my life ha!",
+			expected: []string{"this", "is", "my", "life", "ha!"},
+		},
+		{
+			input:    "",
+			expected: []string{},
+		},
+		{
+			input:    "jkfjkl;adfjkskla;",
+			expected: []string{"jkfjkl;adfjkskla;"},
+		},
+	}
 
+	for _, test := range cases {
+		actual := cleanInput(test.input)
+
+		if len(actual) != len(test.expected) {
+			t.Errorf("The actual length is not the same as the expected %v/%v", len(actual), len(test.expected))
+		}
+		for i := range actual {
+			word := actual[i]
+			expectedWord := test.expected[i]
+			if word != expectedWord {
+				t.Errorf("The word \"%v\" does not match the expected \"%v\"", word, expectedWord)
+			}
+		}
+	}
 }
